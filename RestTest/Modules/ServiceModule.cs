927ff8bd4f2844ab86ba9a8defe6af36a4155d99
damovisa <info@damianbrady.com.au>
2014-07-07 09:02:18 +1000

    Initial project commit

---
+++ RestTest/Modules/ServiceModule.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using Nancy;
+using Nancy.Responses;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace RestTest.Modules
+{
+    public class ServiceModule : NancyModule
+    {
+        private readonly Storage _storage;
+
+        public ServiceModule(Storage storage) : this()
+        {
+            _storage = storage;
+        }
+
+        public ServiceModule()
+        {
+            Post["/{key}"] = _ => ProcessRequest("POST", _);
+            Get["/{key}"] = _ => ProcessRequest("GET", _);
+            Put["/{key}"] = _ => ProcessRequest("PUT", _);
+            Delete["/{key}"] = _ => ProcessRequest("DELETE", _);
+
+            Get["/{key}/View"] = _ =>
+            {
+                var results = _storage.GetRecords((string)_.key);
+                var resultsViewModel = new ResultsViewModel
+                {
+                    Key = (string)_.key,
+                    Results = results.Select(r => new RequestDataViewModel
+                    {
+                        Key = r.PartitionKey,
+                        RowKey = r.RowKey,
+                        RequestTime = r.RequestTime.ToString("G"),
+                        Body = FormatBody(r.Body, r.Header),
+                        Header = r.Header,
+                        Verb = r.Verb
+                    })
+                };
+                return View["View2", resultsViewModel];
+            };
+
+            Get["/{key}/Clear"] = _ =>
+            {
+                _storage.DeleteAll((string) _.key);
+                return "OK";
+            };
+
+            Get["/Dump"] = _ =>
+            {
+                _storage.DumpTable();
+                return "Ok, dumped everything";
+            };
+        }
+
+        private string FormatBody(string body, string header)
+        {
+            if (header.ToLower().Contains("application/xml") || header.ToLower().Contains("text/xml") || header.ToLower().Contains("application/soap+xml"))
+            {
+                try
+                {
+                    XDocument doc = XDocument.Parse(body);
+                    return doc.ToString();
+                }
+                catch (Exception)
+                {
+                    // oh well, we tried
+                }
+            }
+            else if (header.ToLower().Contains("application/json"))
+            {
+                try
+                {
+                    return JsonConvert.SerializeObject(JsonConvert.DeserializeObject(body), Formatting.Indented);
+                }
+                catch (Exception)
+                {
+                    // eh, we tried
+                }
+            }
+            return body;
+        }
+
+        private TextResponse ProcessRequest(string verb, dynamic _)
+        {
+            var key = (string)_.key;
+            var req = new RequestData(key)
+            {
+                Body = new StreamReader(Request.Body).ReadToEnd(),
+                Header = string.Join("\n", Request.Headers.Select(r => r.Key + ": " + string.Join(", ", r.Value))),
+                Verb = verb,
+                RequestTime = DateTime.Now
+            };
+
+            _storage.WriteRecord(req);
+
+            return new TextResponse();
+        }
+    }
+
+    public class ResultsViewModel
+    {
+        public string Key { get; set; }
+        public IEnumerable<RequestDataViewModel> Results { get; set; }
+    }
+    public class RequestDataViewModel
+    {
+        public string Key { get; set; }
+        public string RowKey { get; set; }
+        public string RequestTime { get; set; }
+        public string Verb { get; set; }
+
+        public string Header { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ RestTest/Storage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace RestTest
+{
+    public class Storage
+    {
+        private readonly CloudStorageAccount _account;
+
+        public Storage()
+        {
+            var useEmulated = true;
+            bool.TryParse(ConfigurationManager.AppSettings["UseEmulatedStorage"], out useEmulated);
+            _account = CloudStorageAccount.Parse(
+                useEmulated ?
+                ConfigurationManager.AppSettings["EmulatedConnectionString"] :
+                ConfigurationManager.AppSettings["StorageConnectionString"]);
+        }
+
+        public bool WriteRecord(RequestData request)
+        {
+            var tableClient = _account.CreateCloudTableClient();
+
+            var table = tableClient.GetTableReference("requests");
+            table.CreateIfNotExists();
+
+            var operation = TableOperation.Insert(request);
+
+            try
+            {
+                table.Execute(operation);
+                return true;
+            }
+            finally
+            {
+            }
+            return false;
+        }
+
+        public IEnumerable<RequestData> GetRecords(string key, int take = 5)
+        {
+            var tableClient = _account.CreateCloudTableClient();
+
+            var table = tableClient.GetTableReference("requests");
+
+            TableQuery<RequestData> query = new TableQuery<RequestData>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey",QueryComparisons.Equal,key))
+                .Take(take);
+
+            return table.ExecuteQuery(query);
+        }
+
+        public void DeleteAll(string key)
+        {
+            var tableClient = _account.CreateCloudTableClient();
+
+            var table = tableClient.GetTableReference("requests");
+
+            TableQuery<RequestData> query = new TableQuery<RequestData>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, key));
+
+            var batchDelete = new TableBatchOperation();
+            foreach (var entity in table.ExecuteQuery(query))
+            {
+                batchDelete.Add(TableOperation.Delete(entity));
+            }
+            table.ExecuteBatch(batchDelete);
+        }
+
+        public void DumpTable()
+        {
+            var tableClient = _account.CreateCloudTableClient();
+
+            var table = tableClient.GetTableReference("requests");
+
+            table.Delete();
+        }
+    }
+
+    public class RequestData : TableEntity
+    {
+        public RequestData(string key)
+        {
+            PartitionKey = key;
+            RowKey = (DateTime.MaxValue.Ticks - DateTime.Now.Ticks).ToString("{0:d20}");
+        }
+
+        public RequestData() { }
+
+        public DateTime RequestTime { get; set; }
+        public string Verb { get; set; }
+
+        public string Header { get; set; }
+
+        public string Body { get; set; }
+    }
+}
